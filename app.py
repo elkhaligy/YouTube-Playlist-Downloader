921eaad2e2c138a8fda7739c1f555d25a9f6a349
sirito97 <siritok100@gmail.com>
2021-11-16 21:04:08 +0200

    More formatting

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from tkinter import filedialog
 from pytube import YouTube
 from pytube import Playlist
 from PIL import ImageTk, Image
+
 import urllib.request
 
 # Initializing  urllib library for openning URLs
@@ -13,6 +14,7 @@ opener.addheaders = [(
     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36",
 )]
 urllib.request.install_opener(opener)
+# ~~~~
 
 
 # Functions
@@ -35,18 +37,38 @@ def open_save_location():
     print(folder_name)
 
 
-def fetch_thumbnail():
-    global my_img
-    print(video_link_entry.get())
-    video_link = video_link_entry.get()
-    video = YouTube(video_link)
-    urllib.request.urlretrieve(video.thumbnail_url,  # nosec
-                               "video1_thumbnail.png")  # nosec
-    image = Image.open("video1_thumbnail.png")
-    img = image.resize((300, 200))
-    my_img = ImageTk.PhotoImage(img)
-    my_label = Label(image=my_img)
-    my_label.grid(row=0, column=0)
+def fetch_info():
+    if(r.get() == 1):
+        global my_img
+        global vid_length
+        print(video_link_entry.get())
+        video_link = video_link_entry.get()
+        video = YouTube(video_link)
+        urllib.request.urlretrieve(  # nosec
+            video.thumbnail_url, "video1_thumbnail.png")  # nosec
+        image = Image.open("video1_thumbnail.png")
+        img = image.resize((300, 200))
+        my_img = ImageTk.PhotoImage(img)
+        my_label_img = Label(image=my_img)
+        my_label_img.grid(row=1, column=0)
+        # print(type(video.streams.filter(progressive=True)))
+
+        vid_length = video.length
+        length_label = Label(
+            root, text=f"Video length: {vid_length/60} min ")
+        length_label.grid()
+        
+        vid_title = video.title
+        title_label = Label(
+            root, text=f"Video title: {vid_title}")
+        title_label.grid()
+        
+        vid_views = video.views
+        views_label = Label(
+            root, text=f"Video title: {vid_views}")
+        views_label.grid()
+        print(video.streams)
+# ~~~~
 
 
 # Main loop
@@ -54,22 +76,29 @@ root = Tk()
 root.title("YouTube Video Downloader")
 root.geometry("")
 
+r = IntVar()
+r.set("1")
+Radiobutton(root, text="Video", variable=r, value=1).grid(row=0, column=0)
+Radiobutton(root, text="Playlist", variable=r, value=2).grid(row=0, column=1)
+
 # Global variables
 folder_name = ""
 my_img = ""
+vid_length = ""
 # ~~~
 
 # Video URL input
-text_1 = Label(root, text="Enter Video URL", font=("jost", 15))
-text_1.grid()
+url_label = Label(root, text="Enter Video URL", font=("jost", 15))
+url_label.grid()
 video_link_entry = Entry(root, width=50, borderwidth=1)
 video_link_entry.grid()
-get_thumbnail = Button(root,
-                       text="Submit",
-                       bg="white",
-                       fg="black",
-                       command=fetch_thumbnail)
-get_thumbnail.grid()
+submit_url = Button(
+    root,
+    text="Submit",
+    bg="white",
+    fg="black",
+    command=fetch_info)
+submit_url.grid()
 # ~~~
 
 # Save location input
@@ -84,19 +113,20 @@ choose_location.grid()
 # ~~~
 
 # Video quality input
-text_3 = Label(root, text="Select quality", font=("jost", 15))
-text_3.grid()
+quality_label = Label(root, text="Select quality", font=("jost", 15))
+quality_label.grid()
 quality_options = ["720p", "144p"]
 quality_options_choose = ttk.Combobox(root, values=quality_options)
 quality_options_choose.grid()
 # ~~~
 
 # Download button input
-download_button = Button(root,
-                         text="Download",
-                         bg="red",
-                         fg="white",
-                         command=download_video)
+download_button = Button(
+    root,
+    text="Download",
+    bg="red",
+    fg="white",
+    command=download_video)
 download_button.grid()
 # ~~~
 
