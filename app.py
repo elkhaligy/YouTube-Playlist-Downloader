0e5d05996cd65ce700663d1dd2e3455a77991925
sirito97 <siritok100@gmail.com>
2021-11-16 22:46:09 +0200

    Download threading & Progress indicator

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+
 from pytube import YouTube
 from pytube import Playlist
+
 from PIL import ImageTk, Image
+import threading
+#from pytube.cli import on_progress
 
 import urllib.request
 
@@ -17,31 +21,53 @@ urllib.request.install_opener(opener)
 # ~~~~
 
 
+# Main loop
+root = Tk()
+root.title("YouTube Video Downloader")
 # Functions
+
+
 def download_video():
+    print("hello")
+    global stream
+    """This function starts to download the choosen video"""
     choice = quality_options_choose.get()
     url = video_link_entry.get()
-    video = YouTube(url)
+    video = YouTube(url, on_progress_callback=on_progress)
     if choice == quality_options[0]:
-        stream = video.streams.filter(progressive=True).first()
+        #stream = video.streams.filter(progressive=True).first()
+        stream = video.streams.get_highest_resolution()
     elif choice == quality_options[1]:
         stream = video.streams.filter(progressive=True).last()
     stream.download(folder_name)
 
 
+def on_progress(chunk: bytes, file_handler, bytes_remaining: int):
+    global progress
+    bytes_downloaded = stream.filesize-bytes_remaining
+    print(stream.filesize)
+    print(stream.filesize-bytes_remaining)
+    progress = (bytes_downloaded/stream.filesize)*100
+    print(progress)
+    my_label = Label(root, text=f"progress {progress}%")
+    my_label.pack()
+
+
 def open_save_location():
+    """This function opens a folder browser to choose where to save your video or playlist"""
     global folder_name
     folder_name = filedialog.askdirectory()
     folder_name_print = Label(root, text=folder_name)
-    folder_name_print.grid()
+    folder_name_print.pack()
     print(folder_name)
 
 
 def fetch_info():
+    """This function draws video info like thumbnail, length and views"""
     if(r.get() == 1):
         global my_img
         global vid_length
-        print(video_link_entry.get())
+        # print(video_link_entry.get())
         video_link = video_link_entry.get()
         video = YouTube(video_link)
         urllib.request.urlretrieve(  # nosec
@@ -50,38 +76,36 @@ def fetch_info():
         img = image.resize((300, 200))
         my_img = ImageTk.PhotoImage(img)
         my_label_img = Label(image=my_img)
-        my_label_img.grid(row=1, column=0)
+        my_label_img.pack()
         # print(type(video.streams.filter(progressive=True)))
 
         vid_length = video.length
         length_label = Label(
             root, text=f"Video length: {vid_length/60} min ")
-        length_label.grid()
-        
+        length_label.pack()
+
         vid_title = video.title
         title_label = Label(
             root, text=f"Video title: {vid_title}")
-        title_label.grid()
-        
+        title_label.pack()
+
         vid_views = video.views
         views_label = Label(
             root, text=f"Video title: {vid_views}")
-        views_label.grid()
-        print(video.streams)
+        views_label.pack()
+        # print(video.streams)
+
+
 # ~~~~
 
 
-# Main loop
-root = Tk()
-root.title("YouTube Video Downloader")
-root.geometry("")
-
 r = IntVar()
 r.set("1")
-Radiobutton(root, text="Video", variable=r, value=1).grid(row=0, column=0)
-Radiobutton(root, text="Playlist", variable=r, value=2).grid(row=0, column=1)
+Radiobutton(root, text="Video", variable=r, value=1).pack()
+Radiobutton(root, text="Playlist", variable=r, value=2).pack()
 
 # Global variables
+progress = ""
 folder_name = ""
 my_img = ""
 vid_length = ""
@@ -89,16 +113,16 @@ vid_length = ""
 
 # Video URL input
 url_label = Label(root, text="Enter Video URL", font=("jost", 15))
-url_label.grid()
+url_label.pack()
 video_link_entry = Entry(root, width=50, borderwidth=1)
-video_link_entry.grid()
+video_link_entry.pack()
 submit_url = Button(
     root,
     text="Submit",
     bg="white",
     fg="black",
     command=fetch_info)
-submit_url.grid()
+submit_url.pack()
 # ~~~
 
 # Save location input
@@ -109,15 +133,15 @@ choose_location = Button(
     fg="black",
     command=open_save_location,
 )
-choose_location.grid()
+choose_location.pack()
 # ~~~
 
 # Video quality input
 quality_label = Label(root, text="Select quality", font=("jost", 15))
-quality_label.grid()
+quality_label.pack()
 quality_options = ["720p", "144p"]
 quality_options_choose = ttk.Combobox(root, values=quality_options)
-quality_options_choose.grid()
+quality_options_choose.pack()
 # ~~~
 
 # Download button input
@@ -126,9 +150,10 @@ download_button = Button(
     text="Download",
     bg="red",
     fg="white",
-    command=download_video)
-download_button.grid()
+    command=lambda: threading.Thread(target=download_video).start())
+download_button.pack()
 # ~~~
+
 
 root.mainloop()
 # ~~~~
